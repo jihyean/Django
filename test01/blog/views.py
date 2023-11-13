395e5b28f3b883008e9066e05fcc6ee5205ec494
jihyean <d9rmlatte@gmail.com>
2023-11-13 13:02:13 +0900

    장고 게시판 구현

---
+++ test01/blog/views.py
@@ -0,0 +1,7 @@
+# test01 \ blog \ views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1> hi </h1>")
